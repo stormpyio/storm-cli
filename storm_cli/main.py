028d578b170a48157cc88fd782a2f61d7fbe7ebc
Adib Grouz <contact@adib-grouz.com>
2024-09-09 18:34:33 +0200

    add cmds to main

--- storm_cli/main.py
+++ storm_cli/main.py
@@ -1,11 +1,14 @@
 import typer
-from storm_cli.commands import new
-from storm_cli.commands import generate
+from storm_cli.commands import new, generate, build, test
 
 app = typer.Typer(help="Storm CLI - A command-line tool for managing Storm applications.")
 
 # Register commands
 app.add_typer(generate.app, name="generate")
+app.add_typer(build.app, name="build")
+app.add_typer(test.app, name="test")
+app.add_typer(new.app, name="new")
+
 
 if __name__ == "__main__":
     app()
