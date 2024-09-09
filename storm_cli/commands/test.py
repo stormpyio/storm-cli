b6bafa5268aa7058f353216ca366a2fb6e1f8809
Adib Grouz <contact@adib-grouz.com>
2024-09-09 18:12:01 +0200

    add test cmd

---
+++ storm_cli/commands/test.py
@@ -0,0 +1,17 @@
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def run():
+    """
+    Run all tests.
+
+    :return: None
+    """
+    typer.echo("Running all tests...")
+    # TODO: Integrate with a testing framework to run the tests, e.g., pytest.
+    typer.echo("Tests complete!")
+
+if __name__ == "__main__":
+    app()
